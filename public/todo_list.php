82c2fd218b5a4af6853cf0b55e3ef28aa90923ca
J.D. Garza <jdgarza22@yahoo.com>
2015-02-09 15:36:01 -0600

    updated todo and css file

--- public/todo_list.php
+++ public/todo_list.php
@@ -2,11 +2,15 @@
 var_dump($_POST);
  ?>
 <html>
-	<head>
-		<title>TODO List</title>
-	</head>
+<head>
+	<title>TODO List</title>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="/todo.css">
+	
+</head>
 <body>
 	<h1>TODO List</h1>
+
 		<ul>
 			<li>Wash the car</li>
 			<li>Clean the kitchen</li>
@@ -18,10 +22,10 @@ var_dump($_POST);
 			<p>
 				<label for="todo_today">What else do you have to do today?</label>
 				<input type="text" id="todo_today" name="todo_today">
-
-					<input type="submit">
+				<input type="submit">
 			</p>
 		</form>
 
 </body>
 </html>
+
